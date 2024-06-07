2314be3a63452db2f88161809316037336bcd4e8
Quentin Vilcoque <q.vilcoque@gmail.com>
2024-06-07 14:31:10 +0200

    model

--- webapp_API/my_app/api.py
+++ webapp_API/my_app/api.py
@@ -23,26 +23,6 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 
-@api.route("/api/greet", methods=["GET"])
-def greet():
-    return jsonify(message="coucou")
-
-
-@api.route("/api/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return jsonify(message="Aucun fichier trouvé"), 400
-
-    f = request.files["file"]
-    if f.filename == "":
-        return jsonify(message="Aucun fichier sélectionné"), 400
-
-    filename = secure_filename(f.filename)
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
-    f.save(filepath)
-    app_base_url = os.getenv("APP_BASE_URL")
-    return redirect(f"{app_base_url}/")
-
 model_path = 'src/model/cat_classifier.h5'
 model = load_model(model_path)
 
@@ -50,14 +30,15 @@ model = load_model(model_path)
 @api.route('/api/predict', methods=['POST'])
 def predict():
     try:
-        # Vérifier si le fichier est dans la requête
         if 'file' not in request.files:
-            return 'No file provided', 400
+            return jsonify({'error': 'No file provided'}), 400
 
         file = request.files['file']
+        filepath = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
+        file.save(filepath)
 
         # Prétraiter l'image
-        image = load_img(file, target_size=(224, 224))
+        image = load_img(filepath, target_size=(224, 224))
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
         image = image / 255.0
@@ -65,6 +46,9 @@ def predict():
         # Faire la prédiction
         prediction = model.predict(image)
         is_cat = prediction[0][0] > 0.5
+
+        # Supprimer l'image après la prédiction
+        os.remove(filepath)
 
         # Retourner la prédiction dans une page HTML
         return render_template('result.html', is_cat=is_cat)
