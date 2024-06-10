767130069102ae987bc49928d8978a6a89bc4d10
Quentin Vilcoque <q.vilcoque@gmail.com>
2024-06-10 10:43:57 +0200

    Update app.py

--- webapp_API/my_app/app.py
+++ webapp_API/my_app/app.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 def home():
     base_url = os.getenv('APP_URL')  # Valeur par défaut si la variable n'est pas définie
     api_url = os.getenv('API_URL')  # Valeur par défaut si la variable n'est pas définie
-    is_cat = True  # Remplacez ceci par votre logique réelle
-    return render_template('index.html', is_cat=is_cat, base_url=base_url, api_url=api_url)
+    return render_template('index.html', base_url=base_url, api_url=api_url)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
