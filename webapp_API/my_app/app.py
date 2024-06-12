97d392ed4313622bc53ce0b62323870dab7d6a13
Quentin Vilcoque <q.vilcoque@gmail.com>
2024-06-12 10:15:06 +0200

    update

--- webapp_API/my_app/app.py
+++ webapp_API/my_app/app.py
@@ -3,15 +3,15 @@ from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 
-load_dotenv()  # Charger les variables d'environnement à partir du fichier .env
+load_dotenv()  
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    base_url = os.getenv('APP_URL')  # Valeur par défaut si la variable n'est pas définie
-    api_url = os.getenv('API_URL')  # Valeur par défaut si la variable n'est pas définie
+    base_url = os.getenv('APP_URL')  
+    api_url = os.getenv('API_URL')  
     return render_template('index.html', base_url=base_url, api_url=api_url)
 
 if __name__ == "__main__":
